e877d63ea0d91a1087a677d2ad3c8596f7ee3645
Roman Tsymbaliuk <rts100@cambiumnetworks.com>
2021-07-19 15:04:21 +0300

    Bug fixing

--- c_file/Arguments.c
+++ c_file/Arguments.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdarg.h>
-int sum_counter(int argc, ...){
-	
-va_list ap;
-va_start(ap, argc);
-int sum = 0;
-for (int i = 1; i<=argc; i++){
-int a = va_arg(ap, int);
-sum+=a;
-}
-return sum;
+
+int sum_counter(int argc, ...){	
+	va_list ap;
+	va_start(ap, argc);
+	int sum = 0;
+	for (int i = 1; i<=argc; i++){
+		int a = va_arg(ap, int);
+		sum+=a;
+	}
+	return sum;
 }
 int main(int argc, char* argv[]){
-printf("\n%d", sum_counter(5, 1, -2, 3, 8, 87));
-return 0;
+	printf("\n%d", sum_counter(5, 1, -2, 3, 8, 87));
+	return 0;
 }
