3f8cb71181d4901f08fea428cc0293513f8702a4
Roman Tsymbaliuk <rts100@cambiumnetworks.com>
2021-07-19 18:27:07 +0300

    Style fixing one more time...

--- c_file/TwoOptional.c
+++ c_file/TwoOptional.c
@@ -3,13 +3,11 @@
 #include <getopt.h>
 
 static int verbose_flag;
-
 int main (int argc, char **argv)
 {
 	int c;
 
-	while (1)
-	{
+	while (1) {
 		static struct option long_options[] =
 		{
 			  {"verbose", no_argument,       &verbose_flag, 1},
@@ -23,51 +21,55 @@ int main (int argc, char **argv)
 		};
 		int option_index = 0;
 
-		c = getopt_long (argc, argv, "abc:d:f:", long_options, &option_index);
+		c = getopt_long(argc, argv, "abc:d:f:", long_options, 
+		&option_index);
 
-		if (c == -1)
+		if (c == -1) {
+			break;
+		}
+
+		switch (c) {
+		case 0:
+			if (long_options[option_index].flag != 0) {
+		 		break;
+			}
+			printf("option %s", long_options[option_index].name);
+			if (optarg) {
+		 		printf (" with arg %s", optarg);
+			}
+			printf ("\n");
 			break;
 
-		switch (c)
-		{
-			case 0:
-				if (long_options[option_index].flag != 0)
-			 		break;
-				printf ("option %s", long_options[option_index].name);
-				if (optarg)
-			 		printf (" with arg %s", optarg);
-				printf ("\n");
-				break;
+		case 'a':
+	        	puts ("option -a\n");
+	        	break;
 
-			case 'a':
-		        	puts ("option -a\n");
-		        	break;
+		case 'b':
+			puts ("option -b\n");
+			break;
 
-			case 'b':
-				puts ("option -b\n");
-				break;
+		case 'c':
+			printf ("option -c with value `%s'\n", optarg);
+			break;
 
-			case 'c':
-				printf ("option -c with value `%s'\n", optarg);
-				break;
+		case 'd':
+			printf ("option -d with value `%s'\n", optarg);
+			break;
 
-			case 'd':
-				printf ("option -d with value `%s'\n", optarg);
-				break;
+		case 'f':
+			printf ("option -f with value `%s'\n", optarg);
+			break;
 
-			case 'f':
-				printf ("option -f with value `%s'\n", optarg);
-				break;
+		case '?':
+			break;
 
-			case '?':
-				break;
-
-			default:
-				abort ();
+		default:
+			abort ();
 		}
 	}
 
-	if (verbose_flag)
+	if (verbose_flag) {
 		puts ("verbose flag is set");
+	}
 
 }
--- c_file/ascii.c
+++ c_file/ascii.c
@@ -2,11 +2,11 @@
 
 int main(){
 	printf("ASCII TABLE FOR ALPHABET\n");
-	for (int i = 0; i < 127; i++){
-		if (i < 32 || i==127){
+	for (int i = 0; i < 127; i++) {
+		if (i < 32 || i==127) {
 			printf("\ndec : %d hex : %x - char : NON-READABLE", i, i);
 		}
-		else{
+		else {
 			printf("\ndec : %d hex : %x - char : %c", i, i, i);
 		}
 	}
