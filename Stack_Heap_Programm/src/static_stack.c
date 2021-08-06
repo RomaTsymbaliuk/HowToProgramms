74d4c88d58dd2b6dfe43bcd945b5a4715af28076
Roman Tsymbaliuk <rts100@cambiumnetworks.com>
2021-08-06 10:48:32 +0300

    Deleting old folders

---
+++ Stack_Heap_Programm/src/static_stack.c
@@ -0,0 +1,94 @@
+#include "static_stack.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+
+int static_stack_init(struct data *d)
+{
+
+	(d->anon_s).structure_pointer = &Static_Stack;
+	struct static_stack *st = malloc(sizeof(struct static_stack));
+	st->top = -1;
+	static char *arr[MAX_SIZE] = {""};
+	st->arr = arr;
+	(d->anon_s).structure_pointer = st;
+
+	return TRUE;
+}
+int static_stack_push(struct data *d)
+{
+	
+	struct static_stack *st = (d->anon_s).structure_pointer;
+	if (static_stack_is_full(d) == FALSE) {
+		st->arr[++st->top] = d->data_ptr;
+
+		return TRUE;
+	}
+
+	return FALSE;
+}
+int static_stack_is_empty(struct data *d)
+{
+	
+	struct static_stack *st = d->anon_s.structure_pointer;
+	if (st->top == -1) {
+
+		return TRUE;
+	} else {
+
+		return FALSE;
+	}
+	
+}
+int static_stack_pop(struct data *d)
+{
+
+	struct static_stack *st = d->anon_s.structure_pointer;
+	
+	if (static_stack_is_empty(d) == FALSE) {
+		st->arr[st->top--] = NULL;
+	}
+	
+	return TRUE;
+	
+}
+int static_stack_is_full(struct data *d)
+{
+	
+	struct static_stack *st = d->anon_s.structure_pointer;
+		if (st->top == MAX_SIZE) {
+		return TRUE;
+	}
+	return FALSE;
+	
+}
+int static_stack_print(struct data *d)
+{
+
+	struct static_stack *st = d->anon_s.structure_pointer;
+	FILE *stream;
+	if (d->filename) {
+		FILE *f = fopen(d->filename, "w");
+		if (!f)
+			return FALSE;
+		stream = f;
+	}
+	else {
+		stream = stdout;
+	}
+	for (int i = 0; i <= st->top; i++) {
+		fprintf(stream, "\n%s\n", st->arr[i]);
+	}
+	if (stream)
+		fclose(stream);
+	return TRUE;
+}
+int static_stack_upload(struct data *d)
+{
+	return TRUE;
+}
+int static_stack_download(struct data *d)
+{
+	return TRUE;
+}
